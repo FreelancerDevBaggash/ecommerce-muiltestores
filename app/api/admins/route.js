75ca241c6883b4aeda83fc577e51df19734234cd
FreelancerDevBaggash <127331189+FreelancerDevBaggash@users.noreply.github.com>
2025-05-10 04:16:26 +0300

    Fix: fix api error admin

--- app/api/admins/route.js
+++ app/api/admins/route.js
@@ -1,27 +1,29 @@
-// import db from "@/lib/db"
-// import { NextResponse } from "next/server";
+import db from "@/lib/db"
+import { NextResponse } from "next/server";
 
-// export async function GET(request, {params:{id}}){
-//     try{
-//         const admin = await db.admin.findUnique({
-//             where:{
-//                 id
-//             },
-//         });
-//         return NextResponse.json(admin)
-//     }catch(error){
-//             console.log(error);
-//             return NextResponse.json(
-//         {
-//             message: "Failed to Fetch Admin",
-//             error,
-//         },{status:500}
+export async function GET({request}){
+    const{id} = await request.json();
+
+    try{
+        const admin = await db.admin.findUnique({
+            where:{
+                id
+            },
+        });
+        return NextResponse.json(admin)
+    }catch(error){
+            console.log(error);
+            return NextResponse.json(
+        {
+            message: "Failed to Fetch Admin",
+            error,
+        },{status:500}
         
-//             )
+            )
     
-//     }
+    }
     
-//     }
+    }
 
 
 

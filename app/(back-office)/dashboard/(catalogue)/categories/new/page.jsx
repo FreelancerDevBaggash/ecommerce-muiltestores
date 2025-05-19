dc3a740b502d2e4c0fdfc50daae50599ae0ce54e
FreelancerDevBaggash <127331189+FreelancerDevBaggash@users.noreply.github.com>
2025-05-20 01:59:29 +0300

    Fix: fix category  page

--- app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
@@ -3,7 +3,7 @@ export const dynamic = "force-static";
 import React from "react"
 import NewCategoryForm from "../../../../../../components/backoffice/NewCategoryForm";
 import {getData} from "../../../../../../lib/getData";
-import { getServerSession } from 'next-auth';
+import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/lib/authOptions';
 
 export default async function NewCategory(){
